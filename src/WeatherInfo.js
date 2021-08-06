1cfe5facc55514d452343746ebdcdc498c7be2f1
celonen <84405152+celonen@users.noreply.github.com>
2021-08-06 09:11:47 +0200

    Added unit coverter: celsius to fahrenheit temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
-import Temperature from "./Temperature";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -21,7 +21,7 @@ export default function WeatherInfo(props) {
             </div>
 
             <div className="float-left">
-              <Temperature celsius={props.data.temperature} />
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
