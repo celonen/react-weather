4893a03b27e2ead794c187c74ba8ed37578e2ee5
celonen <84405152+celonen@users.noreply.github.com>
2021-08-08 15:49:40 +0200

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import Weather from "./Weather";
-import City from "./City";
-import Temperature from "./Temperature";
-import Conditions from "./Conditions";
-import Forecast from "./Forecast";
+import "./index.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="weather-app-wrapper">
-          <div className="weather-app">
-            <Weather />
-            <br />
-            <City />
-            <br />
-            <div className="row">
-              <div className="col">
-                <Temperature value={19} />
-              </div>
-              <div className="col">
-                <Conditions humidity={80} wind={10} />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <div className="weather-forecast" id="forecast">
-                  <Forecast />
-                </div>
-              </div>
-            </div>
-          </div>
-          <small>
-            <a
-              href="https://github.com/celonen/react-weather"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Open-source code
-            </a>{" "}
-            by Cassandra Elonen
-          </small>
-        </div>
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by Cassandra Elonen and is{" "}
+          <a
+            href="https://github.com/celonen/react-weather"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://confident-lumiere-7b31de.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
